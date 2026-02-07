9a7cb020762990a61253fce96a75b76e615cbb22
Charlene Hsueh <CiaCia@MacBook-Pro.local>
2026-02-06 16:46:01 -0800

    Intake :)

--- src/main/java/frc/robot/Subsystems/Intake/Intake.java
+++ src/main/java/frc/robot/Subsystems/Intake/Intake.java
@@ -1,5 +1,40 @@
 package frc.robot.Subsystems.Intake;
 
+import org.littletonrobotics.junction.Logger;
 public class Intake {
-    
+
+   private final IntakeIO intakeIO;
+   private IntakeIOInputsAutoLogged inputs = new IntakeIOInputsAutoLogged();
+   private double setpointVolts;
+   private IntakeStates intakeState = IntakeStates.IDLE;
+
+   public enum IntakeStates {
+        IDLE
+   }
+
+   public Intake(IntakeIO intakeIO) {
+        this.intakeIO = intakeIO;
+   }
+
+   public void Loop() {
+    intakeIO.updateInputs(inputs);
+    Logger.processInputs("Intake", inputs);
+    Logger.recordOutput("Intake", this.intakeState);
+    switch(intakeState){
+        case IDLE:
+            intakeIO.requestPivotVoltage(0);
+            intakeIO.requestIntakeVoltage(0);
+        default:
+            break;
+    }
+   }
+
+   public void requestIdle() {
+        setState(IntakeStates.IDLE);
+   }
+
+   public void setState(IntakeStates nextState) {
+     this.intakeState = nextState;
+   }
+
 }
--- src/main/java/frc/robot/Subsystems/Intake/IntakeIO.java
+++ src/main/java/frc/robot/Subsystems/Intake/IntakeIO.java
@@ -1,5 +1,34 @@
 package frc.robot.Subsystems.Intake;
 
-public class IntakeIO {
+import org.littletonrobotics.junction.AutoLog;
+
+public interface IntakeIO {
+    @AutoLog
+    public static class IntakeIOInputs {
+        public double pivotAppliedVolts = 0;
+        public double pivotCurrent = 0;
+        public double pivotSetpointDeg = 0;
+        public double pivotSetpointRot = 0;
+        public double pivotPosDeg = 0;
+        public double pivotPosRot = 0;
+        public double pivotTemperature = 0;
+        public double pivotRPS = 0;
+
+        public double intakeTemperature = 0;
+        public double intakeAppliedVolts = 0;
+        public double intakeCurrent = 0;
+        public double intakeRPS = 0;
+        public double intakeSetpointVolts = 0;
+    }
+
+    public default void updateInputs (IntakeIOInputs inputs) {}
+
+    public default void requestPivotVoltage (double voltage) {}
+
+    public default void requestSetpoint (double angleDegrees) {}
     
+    public default void requestIntakeVoltage(double voltage) {}
+
+    public default void zeroPosition() {}
+
 }
